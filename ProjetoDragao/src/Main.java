4dc89202d604f535c7961845159755d5b7a1d5a9
Leonardo <leonardo-f98@hotmail.com>
2023-03-28 12:21:08 -0300

    Ultimo Exercício Primeira Parte

--- ProjetoDragao/src/Main.java
+++ ProjetoDragao/src/Main.java
@@ -1,23 +1,24 @@
-import java.util.*;
-public class Main{
+// Para ler e escrever dados em Java, aqui na DIO padronizamos da seguinte forma:
+// - new Scanner(System.in): cria um leitor de Entradas, com métodos úteis com prefixo "next";
+// - System.out.println:.imprime um texto de Saída (Output) e pulando uma linha.
 
-    public static void main(String[] args){
-        int casos, poderDeLuta;
+import java.io.IOException;
+import java.util.Scanner;
 
-        Scanner ler = new Scanner(System.in);
+public class Teste {
 
-        casos = ler.nextInt();
-
-        for(int i = 0; i < casos; i++){
-            poderDeLuta = ler.nextInt();
-
-//TODO: Implemente a condição adequada para a impressão dos textos conforme o solicitado no desafio:
-
-            if(poderDeLuta <= 8000){
-                System.out.println("Inseto!");
-            } else {
-                System.out.println("Mais de 8000!");
-            }
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int proximo, anterior = 0, atual = 1;
+        for (int i = 1; i <= N; i++) {
+            if (i == N) {
+                System.out.println(anterior);
+            } else System.out.print(anterior + " ");
+            proximo = anterior + atual;
+            anterior = atual - 1 ;
+            atual =  ;
         }
     }
+
 }
